5e4f44b2700d2fcbcf629a7e0bb0caefd4352dcd
Siddharth Barahalikar <barahalikar.siddharth@gmail.com>
2019-12-04 16:31:00 +0530

    Update EmployeeController.java

--- src/main/java/com/openshift/jenkins/EmployeeController.java
+++ src/main/java/com/openshift/jenkins/EmployeeController.java
@@ -21,7 +21,7 @@ public class EmployeeController {
 
     @GetMapping("/info")
     public String info() {
-        return "Service UP and RUNNING";
+        return "Service UP and RUNNINGggggggggggggggggggggggg";
     }
     
     @GetMapping("/employees")
